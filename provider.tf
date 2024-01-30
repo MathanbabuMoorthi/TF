f2602e119e464dff0d20070a5ed4ff4d244f1d3a
MathanbabuMoorthi <154584669+MathanbabuMoorthi@users.noreply.github.com>
2024-01-30 15:34:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAU6GDV64KTQY7N25N"
-  secret_key = "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"
+  access_key = "AKIAU6GDV64KVGGFIT6S"
+  secret_key = "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"
 }
